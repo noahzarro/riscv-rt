976f328e3d664700d274c3446d5be866033ade09
msc22h12 Noah Zarro (zarron) <msc22h12@badile18.ee.ethz.ch>
2022-11-10 11:03:18 +0100

    changed pointer types once again

--- src/clic.rs
+++ src/clic.rs
@@ -2,41 +2,41 @@ pub mod addresses {
 
     /* CLIC Configuration */
     pub const CLIC_CLICCFG_REG_OFFSET: *mut u32 = 0x0 as *mut u32;
-    pub const CLIC_CLICCFG_NVBITS_BIT: *mut u32 = 0 as *mut u32;
-    pub const CLIC_CLICCFG_NLBITS_MASK: *mut u32 = 0xf as *mut u32;
-    pub const CLIC_CLICCFG_NLBITS_OFFSET: *mut u32 = 1 as *mut u32;
-    pub const CLIC_CLICCFG_NMBITS_MASK: *mut u32 = 0x3 as *mut u32;
-    pub const CLIC_CLICCFG_NMBITS_OFFSET: *mut u32 = 5 as *mut u32;
+    pub const CLIC_CLICCFG_NVBITS_BIT:  u32 = 0 as u32;
+    pub const CLIC_CLICCFG_NLBITS_MASK: u32 = 0xf as  u32;
+    pub const CLIC_CLICCFG_NLBITS_OFFSET:  u32 = 1 as  u32;
+    pub const CLIC_CLICCFG_NMBITS_MASK:  u32 = 0x3 as  u32;
+    pub const CLIC_CLICCFG_NMBITS_OFFSET:  u32 = 5 as  u32;
 
     /* CLIC Information */
     pub const CLIC_CLICINFO_REG_OFFSET: *mut u32 = 0x4 as *mut u32;
-    pub const CLIC_CLICINFO_NUM_INTERRUPT_MASK: *mut u32 = 0x1fff as *mut u32;
-    pub const CLIC_CLICINFO_NUM_INTERRUPT_OFFSET: *mut u32 = 0 as *mut u32;
+    pub const CLIC_CLICINFO_NUM_INTERRUPT_MASK:  u32 = 0x1fff as  u32;
+    pub const CLIC_CLICINFO_NUM_INTERRUPT_OFFSET:  u32 = 0 as  u32;
 
-    pub const CLIC_CLICINFO_VERSION_MASK: *mut u32 = 0xff as *mut u32;
-    pub const CLIC_CLICINFO_VERSION_OFFSET: *mut u32 = 13 as *mut u32;
+    pub const CLIC_CLICINFO_VERSION_MASK:  u32 = 0xff as  u32;
+    pub const CLIC_CLICINFO_VERSION_OFFSET:  u32 = 13 as  u32;
 
-    pub const CLIC_CLICINFO_CLICINTCTLBITS_MASK: *mut u32 = 0xf as *mut u32;
-    pub const CLIC_CLICINFO_CLICINTCTLBITS_OFFSET: *mut u32 = 21 as *mut u32;
+    pub const CLIC_CLICINFO_CLICINTCTLBITS_MASK:  u32 = 0xf as  u32;
+    pub const CLIC_CLICINFO_CLICINTCTLBITS_OFFSET:  u32 = 21 as  u32;
 
-    pub const CLIC_CLICINFO_NUM_TRIGGER_MASK: *mut u32 = 0x3f as *mut u32;
-    pub const CLIC_CLICINFO_NUM_TRIGGER_OFFSET: *mut u32 = 25 as *mut u32;
+    pub const CLIC_CLICINFO_NUM_TRIGGER_MASK:  u32 = 0x3f as  u32;
+    pub const CLIC_CLICINFO_NUM_TRIGGER_OFFSET:  u32 = 25 as  u32;
 
     /* CLIC enable mnxti irq forwarding logic */
     pub const CLIC_CLICXNXTICONF_REG_OFFSET: *mut u32 = 0x8 as *mut u32;
-    pub const CLIC_CLICXNXTICONF_CLICXNXTICONF_BIT: *mut u32 = 0 as *mut u32;
+    pub const CLIC_CLICXNXTICONF_CLICXNXTICONF_BIT: u32 = 0 as  u32;
 
     /* CLIC interrupt id pending */
     pub fn CLIC_CLICINTIP_REG_OFFSET(id: u32) -> *mut u32 {
         (0x1000 + 0x10 * id) as *mut u32
     }
-    pub const CLIC_CLICINTIP_CLICINTIP_BIT: *mut u32 = 0 as *mut u32;
+    pub const CLIC_CLICINTIP_CLICINTIP_BIT: u32 = 0 as u32;
 
     /* CLIC interrupt id enable */
     pub fn CLIC_CLICINTIE_REG_OFFSET(id: u32) -> *mut u32 {
         (0x1004 + 0x10 * id) as *mut u32
     }
-    pub const CLIC_CLICINTIE_CLICINTIE_BIT: *mut u32 = 0 as *mut u32;
+    pub const CLIC_CLICINTIE_CLICINTIE_BIT: u32 = 0 as u32;
 
     /* CLIC interrupt id attributes */
     pub fn CLIC_CLICINTATTR_REG_OFFSET(id: u32) -> *mut u32 {
@@ -57,11 +57,11 @@ pub mod addresses {
     pub fn CLIC_CLICINTCTL_REG_OFFSET(id: u32) -> *mut u32 {
         (0x100c + 0x10 * id) as *mut u32
     }
-    pub const CLIC_CLICINTCTL_CLICINTCTL_MASK: *mut u32 = 0xff as *mut u32;
-    pub const CLIC_CLICINTCTL_CLICINTCTL_OFFSET: *mut u32 = 0 as *mut u32;
+    pub const CLIC_CLICINTCTL_CLICINTCTL_MASK:  u32 = 0xff as  u32;
+    pub const CLIC_CLICINTCTL_CLICINTCTL_OFFSET:  u32 = 0 as  u32;
 
-    pub const CSR_MXNTI_ID: *mut u32 = 0x345 as *mut u32;
-    pub const MIE: *mut u32 = 8 as *mut u32;
+    pub const CSR_MXNTI_ID:  u32 = 0x345 as  u32;
+    pub const MIE:  u32 = 8 as  u32;
 }
 
 pub mod crs {
