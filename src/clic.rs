7acc52c85fa5355bfb1bc128f63cdd7277b02c92
msc22h12 Noah Zarro (zarron) <msc22h12@badile18.ee.ethz.ch>
2022-11-10 10:11:08 +0100

    added clic constants and clic interrupt configuration

--- src/clic.rs
+++ src/clic.rs
@@ -1,62 +1,109 @@
-/* CLIC Configuration */
-pub const CLIC_CLICCFG_REG_OFFSET: u32 = 0x0;
-pub const CLIC_CLICCFG_NVBITS_BIT: u32 = 0;
-pub const CLIC_CLICCFG_NLBITS_MASK: u32 = 0xf;
-pub const CLIC_CLICCFG_NLBITS_OFFSET: u32 = 1;
-pub const CLIC_CLICCFG_NMBITS_MASK: u32 = 0x3;
-pub const CLIC_CLICCFG_NMBITS_OFFSET: u32 = 5;
+pub mod addresses {
 
-/* CLIC Information */
-pub const CLIC_CLICINFO_REG_OFFSET: u32 = 0x4;
-pub const CLIC_CLICINFO_NUM_INTERRUPT_MASK: u32 = 0x1fff;
-pub const CLIC_CLICINFO_NUM_INTERRUPT_OFFSET: u32 = 0;
+    /* CLIC Configuration */
+    pub const CLIC_CLICCFG_REG_OFFSET: u32 = 0x0;
+    pub const CLIC_CLICCFG_NVBITS_BIT: u32 = 0;
+    pub const CLIC_CLICCFG_NLBITS_MASK: u32 = 0xf;
+    pub const CLIC_CLICCFG_NLBITS_OFFSET: u32 = 1;
+    pub const CLIC_CLICCFG_NMBITS_MASK: u32 = 0x3;
+    pub const CLIC_CLICCFG_NMBITS_OFFSET: u32 = 5;
 
-pub const CLIC_CLICINFO_VERSION_MASK: u32 = 0xff;
-pub const CLIC_CLICINFO_VERSION_OFFSET: u32 = 13;
+    /* CLIC Information */
+    pub const CLIC_CLICINFO_REG_OFFSET: u32 = 0x4;
+    pub const CLIC_CLICINFO_NUM_INTERRUPT_MASK: u32 = 0x1fff;
+    pub const CLIC_CLICINFO_NUM_INTERRUPT_OFFSET: u32 = 0;
 
-pub const CLIC_CLICINFO_CLICINTCTLBITS_MASK: u32 = 0xf;
-pub const CLIC_CLICINFO_CLICINTCTLBITS_OFFSET: u32 = 21;
+    pub const CLIC_CLICINFO_VERSION_MASK: u32 = 0xff;
+    pub const CLIC_CLICINFO_VERSION_OFFSET: u32 = 13;
 
-pub const CLIC_CLICINFO_NUM_TRIGGER_MASK: u32 = 0x3f;
-pub const CLIC_CLICINFO_NUM_TRIGGER_OFFSET: u32 = 25;
+    pub const CLIC_CLICINFO_CLICINTCTLBITS_MASK: u32 = 0xf;
+    pub const CLIC_CLICINFO_CLICINTCTLBITS_OFFSET: u32 = 21;
 
-/* CLIC enable mnxti irq forwarding logic */
-pub const CLIC_CLICXNXTICONF_REG_OFFSET: u32 = 0x8;
-pub const CLIC_CLICXNXTICONF_CLICXNXTICONF_BIT: u32 = 0;
+    pub const CLIC_CLICINFO_NUM_TRIGGER_MASK: u32 = 0x3f;
+    pub const CLIC_CLICINFO_NUM_TRIGGER_OFFSET: u32 = 25;
 
-/* CLIC interrupt id pending */
-pub fn CLIC_CLICINTIP_REG_OFFSET(id: u32) -> u32 {
-    0x1000 + 0x10 * id
+    /* CLIC enable mnxti irq forwarding logic */
+    pub const CLIC_CLICXNXTICONF_REG_OFFSET: u32 = 0x8;
+    pub const CLIC_CLICXNXTICONF_CLICXNXTICONF_BIT: u32 = 0;
+
+    /* CLIC interrupt id pending */
+    pub fn CLIC_CLICINTIP_REG_OFFSET(id: u32) -> u32 {
+        0x1000 + 0x10 * id
+    }
+    pub const CLIC_CLICINTIP_CLICINTIP_BIT: u32 = 0;
+
+    /* CLIC interrupt id enable */
+    pub fn CLIC_CLICINTIE_REG_OFFSET(id: u32) -> u32 {
+        0x1004 + 0x10 * id
+    }
+    pub const CLIC_CLICINTIE_CLICINTIE_BIT: u32 = 0;
+
+    /* CLIC interrupt id attributes */
+    pub fn CLIC_CLICINTATTR_REG_OFFSET(id: u32) -> u32 {
+        0x1008 + 0x10 * id
+    }
+    pub const CLIC_CLICINTATTR_SHV_BIT: u32 = 0;
+    pub const CLIC_CLICINTATTR_TRIG_MASK: u32 = 0x3;
+    pub const CLIC_CLICINTATTR_TRIG_OFFSET: u32 = 1;
+    pub const CLIC_CLICINTATTR_MODE_MASK: u32 = 0x3;
+    pub const CLIC_CLICINTATTR_MODE_OFFSET: u32 = 6;
+
+    pub const CLIC_TRIG_LEVEL: u32 = 0;
+    pub const CLIC_TRIG_EDGE: u32 = 1;
+    pub const CLIC_TRIG_POSITIVE: u32 = 0 << 1;
+    pub const CLIC_TRIG_NEGATIVE: u32 = 1 << 1;
+
+    /* CLIC interrupt id control */
+    pub fn CLIC_CLICINTCTL_REG_OFFSET(id: u32) -> u32 {
+        0x100c + 0x10 * id
+    }
+    pub const CLIC_CLICINTCTL_CLICINTCTL_MASK: u32 = 0xff;
+    pub const CLIC_CLICINTCTL_CLICINTCTL_OFFSET: u32 = 0;
+
+    pub const CSR_MXNTI_ID: u32 = 0x345;
+    pub const MIE: u32 = 8;
 }
-pub const CLIC_CLICINTIP_CLICINTIP_BIT: u32 = 0;
 
-/* CLIC interrupt id enable */
-pub fn CLIC_CLICINTIE_REG_OFFSET(id: u32) -> u32 {
-    0x1004 + 0x10 * id
+pub mod crs {
+    use core::arch::asm;
+
+    pub fn write_crs(crs_nr:u32, value:u32){
+        unsafe {
+            asm!(  
+                "csrw {0}, {1}",
+                in(reg) crs_nr,
+                in(reg) value,
+            );   
+        }
+    }
+
+    pub fn read_crs(crs_nr:u32) -> u32{
+        let mut value = 0;
+        unsafe {
+            asm!(  
+                "csrr {0}, {1}",
+                in(reg) crs_nr,
+                out(reg) value,
+            );   
+        }
+        value
+    }
+
+    pub const CSR_MSTATUS: u32 = 0x300;
+    pub const CSR_MISA: u32 = 0x301;
+    pub const CSR_MIE: u32 = 0x304;
+    pub const CSR_MTVEC: u32 = 0x305;
+    pub const CSR_MTVT:u32 = 0x307;
+    pub const CSR_MSCRATCH: u32 = 0x340;
+    pub const CSR_MEPC: u32 = 0x341;
+    pub const CSR_MCAUSE: u32 = 0x342;
+    pub const CSR_MTVAL: u32 = 0x343;
+    pub const CSR_MIP: u32 = 0x344;
+    pub const CSR_MNXTI: u32 = 0x345;
+    pub const CSR_PMPCFG0: u32 = 0x3a0;
+    pub const CSR_PMPADDR0: u32 = 0x3b0;
+    pub const CSR_MHARTID: u32 = 0xf14;
+    pub const CSR_MINTSTATUS: u32 = 0x346;
+    pub const CSR_MINTTHRESH: u32 = 0x347;
+    pub const CSR_MCLICBASE: u32 = 0x350;
 }
-pub const CLIC_CLICINTIE_CLICINTIE_BIT: u32 = 0;
-
-/* CLIC interrupt id attributes */
-pub fn CLIC_CLICINTATTR_REG_OFFSET(id: u32) -> u32 {
-    0x1008 + 0x10 * id
-}
-pub const CLIC_CLICINTATTR_SHV_BIT: u32 = 0;
-pub const CLIC_CLICINTATTR_TRIG_MASK: u32 = 0x3;
-pub const CLIC_CLICINTATTR_TRIG_OFFSET: u32 = 1;
-pub const CLIC_CLICINTATTR_MODE_MASK: u32 = 0x3;
-pub const CLIC_CLICINTATTR_MODE_OFFSET: u32 = 6;
-
-pub const CLIC_TRIG_LEVEL: u32 = 0;
-pub const CLIC_TRIG_EDGE: u32 = 1;
-pub const CLIC_TRIG_POSITIVE: u32 = 0 << 1;
-pub const CLIC_TRIG_NEGATIVE: u32 = 1 << 1;
-
-/* CLIC interrupt id control */
-pub fn CLIC_CLICINTCTL_REG_OFFSET(id: u32) -> u32 {
-    0x100c + 0x10 * id
-}
-pub const CLIC_CLICINTCTL_CLICINTCTL_MASK: u32 = 0xff;
-pub const CLIC_CLICINTCTL_CLICINTCTL_OFFSET: u32 = 0;
-
-pub const CSR_MXNTI_ID: u32 = 0x345;
-pub const MIE: u32 = 8;
