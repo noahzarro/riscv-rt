8b9481688bca866f74178f9d9a82d9237fd9ae49
msc22h12 Noah Zarro (zarron) <msc22h12@badile18.ee.ethz.ch>
2022-12-15 19:11:21 +0100

    try5

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -248,17 +248,15 @@ pub fn interrupt_handler(_args: TokenStream, input: TokenStream) -> TokenStream 
         #(#attrs)*
         pub unsafe fn #handler_ident() #block
 
+        #[no_mangle]
+        pub unsafe fn #ident() {
+            asm!("nop");
+            #handler_ident();
+            asm!("nop");
+        } 
 
     )
     .into()
 
 }
 
-
-/*
-        #[no_mangle]
-        pub unsafe fn #ident() {
-            asm!("nop");
-            #handler_ident();
-            asm!("nop");
-        } */
