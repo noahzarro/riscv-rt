52f67422a545f0e9c4a928561cfd861ec1017ba3
msc22h12 Noah Zarro (zarron) <msc22h12@badile18.ee.ethz.ch>
2022-12-19 14:16:08 +0100

    fixed string ownership issue

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -242,57 +242,59 @@ pub fn interrupt_handler(_args: TokenStream, input: TokenStream) -> TokenStream 
     let attrs = f.attrs;
     let ident = f.sig.ident;
     let block = f.block;
-    let handler_ident = format_ident!("{}_handler", ident);
     let ident_string = ident.to_string();
+    let handler_string = ident_string.clone() + "_handler";
+    let assembly_string = format!(
+    ".global
+    .{ident_string}
+    addi sp, sp, -(4 * 16)
+    sw ra, 0(sp)
+    sw t0, 4(sp)
+    sw t1, 8(sp)
+    sw t2, 12(sp)
+    sw a0, 16(sp)
+    sw a1, 20(sp)
+    sw a2, 24(sp)
+    sw a3, 28(sp)
+    sw a4, 32(sp)
+    sw a5, 36(sp)
+    sw a6, 40(sp)
+    sw a7, 44(sp)
+    sw t3, 48(sp)
+    sw t4, 52(sp)
+    sw t5, 56(sp)
+    sw t6, 60(sp)
+
+    j {handler_string}
+
+    lw ra, 0(sp)
+    lw t0, 4(sp)
+    lw t1, 8(sp)
+    lw t2, 12(sp)
+    lw a0, 16(sp)
+    lw a1, 20(sp)
+    lw a2, 24(sp)
+    lw a3, 28(sp)
+    lw a4, 32(sp)
+    lw a5, 36(sp)
+    lw a6, 40(sp)
+    lw a7, 44(sp)
+    lw t3, 48(sp)
+    lw t4, 52(sp)
+    lw t5, 56(sp)
+    lw t6, 60(sp)
+    addi sp, sp, (4 * 16)
+    mret
+    ");
 
     quote!(
         #(#attrs)*
-        pub unsafe fn #handler_ident() #block
+        pub unsafe fn #handler_string() #block
 
         
             use core::arch::asm;
             use core::arch::global_asm;
-            global_asm!(".global
-            ."+#ident_string+"
-            addi sp, sp, -(4 * 16)
-            sw ra, 0(sp)
-            sw t0, 4(sp)
-            sw t1, 8(sp)
-            sw t2, 12(sp)
-            sw a0, 16(sp)
-            sw a1, 20(sp)
-            sw a2, 24(sp)
-            sw a3, 28(sp)
-            sw a4, 32(sp)
-            sw a5, 36(sp)
-            sw a6, 40(sp)
-            sw a7, 44(sp)
-            sw t3, 48(sp)
-            sw t4, 52(sp)
-            sw t5, 56(sp)
-            sw t6, 60(sp)
-
-            j "+#handler_ident+"
-
-            lw ra, 0(sp)
-            lw t0, 4(sp)
-            lw t1, 8(sp)
-            lw t2, 12(sp)
-            lw a0, 16(sp)
-            lw a1, 20(sp)
-            lw a2, 24(sp)
-            lw a3, 28(sp)
-            lw a4, 32(sp)
-            lw a5, 36(sp)
-            lw a6, 40(sp)
-            lw a7, 44(sp)
-            lw t3, 48(sp)
-            lw t4, 52(sp)
-            lw t5, 56(sp)
-            lw t6, 60(sp)
-            addi sp, sp, (4 * 16)
-            mret
-            ");
+            global_asm!(#assembly_string);
             asm!("nop");
             asm!("nop");
         
